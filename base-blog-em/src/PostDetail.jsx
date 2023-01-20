0cbb37b125b93fdf5e2fbf471e9fb9b62434bf65
roddy.oh <roddy.oh@kakaocorp.com>
2023-01-21 01:16:25 +0900

    Section2: Pagination, Prefetching, and Mutation

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import {useQuery, useMutation} from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -7,7 +9,7 @@ async function fetchComments(postId) {
 
 async function deletePost(postId) {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    `https://jsonplasdfasdfaceholder.typicode.com/postId/${postId}`,
     { method: "DELETE" }
   );
   return response.json();
@@ -23,12 +25,24 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const {data, isLoading, isError} = useQuery(["comments", post.id], () => fetchComments(post.id));
+
+  // replace with useMutation
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId));
+
+  if (isError) return <h3>Error fetching comments</h3>;
+  if (isLoading) return <h3>Loading...</h3>;
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isError && <h3>Error deleting post</h3>}
+      {deleteMutation.isLoading && <h3>Deleting post...</h3>}
+      {deleteMutation.isSuccess && <h3>Post deleted!</h3>}
+      <button onClick={() => updateMutation.mutate(post.id)}>Update title</button>
+      {updateMutation.isSuccess && <h3>Post updated!</h3>}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,26 +1,40 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
+import {useQuery, useQueryClient} from "react-query";
 
-import { PostDetail } from "./PostDetail";
+import {PostDetail} from "./PostDetail";
+
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(page) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const {data, isError, isLoading} = useQuery(["posts", currentPage], () => fetchPosts(currentPage), {staleTime: 2000});
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    const nextPage = currentPage + 1;
+    if (nextPage > maxPostPage) return;
+
+    // prefetch next page
+    queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));
+  }, [currentPage, queryClient]);
+
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError) return <h3>Error fetching posts</h3>;
 
   return (
     <>
       <ul>
-        {data.map((post) => (
+        {data?.map((post) => (
           <li
             key={post.id}
             className="post-title"
@@ -31,16 +45,26 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage(prev => prev - 1)
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage(prev => prev + 1)
+          }}
+        >
           Next page
         </button>
       </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      <hr/>
+      {selectedPost && <PostDetail post={selectedPost}/>}
     </>
   );
 }
